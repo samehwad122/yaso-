85325ed52d23bcca27b77d16b3b6151bc2bfd519
samehwad122 <samehwad2@gmail.com>
2025-02-22 20:48:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let hart = document.querySelector('.hart')
+let mass = document.querySelector('.mass')
+
+
+hart.addEventListener('click' , ()=>{
+    
+    mass.classList.toggle('open')
+    
+    
+})
+
